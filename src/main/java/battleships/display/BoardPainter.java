522ed2ce0026b20a60ea4f1b029064c974e69057
Kesares <kesares19@gmail.com>
2023-06-04 20:28:12 +0200

    Change method order

--- src/main/java/battleships/display/BoardPainter.java
+++ src/main/java/battleships/display/BoardPainter.java
@@ -23,20 +23,20 @@ public class BoardPainter {
         this.adapter.farbe2(event.getX(), event.getY(), XSend.DARKRED);
     }
 
-    private void clear() {
-        for (int y = 0; y < Config.BOARD_HEIGHT; y++) {
-            for (int x = 0; x < Config.BOARD_WIDTH; x++) {
-                this.adapter.farbe2(x, y, Config.BOS_DEFAULT_GRAY);
-            }
-        }
-    }
-
     private void initDraw(Game game) {
         Ship[] ships = game.getShips();
         for (Ship ship : ships) {
             Position[] positions = ship.getPositions();
             for (Position position : positions) {
                 this.adapter.farbe2(position.getX(), position.getY(), XSend.GRAY);
+            }
+        }
+    }
+
+    private void clear() {
+        for (int y = 0; y < Config.BOARD_HEIGHT; y++) {
+            for (int x = 0; x < Config.BOARD_WIDTH; x++) {
+                this.adapter.farbe2(x, y, Config.BOS_DEFAULT_GRAY);
             }
         }
     }
