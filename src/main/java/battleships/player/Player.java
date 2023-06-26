bf8001539c7dd1bad75e99f3b4416619e6241426
Kesares <kesares19@gmail.com>
2023-06-27 00:35:17 +0200

    Implement player class

--- src/main/java/battleships/player/Player.java
+++ src/main/java/battleships/player/Player.java
@@ -1,31 +1,56 @@
 package battleships.player;
 
+import battleships.config.Config;
 import battleships.player.ships.*;
 import battleships.util.Adjust;
 
 @Adjust
 public class Player {
 
-    private String name;
-    private Ship[] ships;
+    private final String name;
+    private final Ship[] ships;
+    private final boolean isPlayer;
+    private final int color;
 
     public Player(String name) {
-        this.name = name;
-        this.ships = new Ship[]{
-                new Battleship(true),
-                new Cruiser(true),
-                new Cruiser(true),
-                new Destroyer(true),
-                new Destroyer(true),
-                new Destroyer(true),
-                new Submarine(true),
-                new Submarine(true),
-                new Submarine(true),
-                new Submarine(true),
-        };
+        this(name, false, Config.SHIP_COLOR_P1);
     }
 
     public Player() {
-        this.name = "AI";
+        this("AI", true, Config.SHIP_COLOR_P2);
+    }
+
+    private Player(String name, boolean isPlayer, int color) {
+        this.name = name;
+        this.isPlayer = isPlayer;
+        this.color = color;
+        this.ships = this.createShips();
+    }
+
+    private Ship[] createShips() {
+        return new Ship[] {
+            new Battleship(this.isPlayer),
+            new Cruiser(this.isPlayer),
+            new Cruiser(this.isPlayer),
+            new Destroyer(this.isPlayer),
+            new Destroyer(this.isPlayer),
+            new Destroyer(this.isPlayer),
+            new Submarine(this.isPlayer),
+            new Submarine(this.isPlayer),
+            new Submarine(this.isPlayer),
+            new Submarine(this.isPlayer),
+        };
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Ship[] getShips() {
+        return ships;
+    }
+
+    public int getColor() {
+        return color;
     }
 }
